9b74634c443155d15d825e4de3ae756c95930f5b
sm18038 <sm18038@ues.edu.sv>
2022-06-02 15:28:54 -0600

    Ultima Prueba Jenkinsfile

--- src/main/java/com/mycompany/baches/control/EstadoBean.java
+++ src/main/java/com/mycompany/baches/control/EstadoBean.java
@@ -2,10 +2,13 @@ package com.mycompany.baches.control;
 
 import com.mycompany.baches.entity.Estado;
 import java.io.Serializable;
+import java.util.Collections;
+import java.util.List;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 @Stateless
 @LocalBean
@@ -22,5 +25,22 @@ public class EstadoBean extends AbstractDataAccess<Estado> implements Serializab
     public EstadoBean() {
         super(Estado.class);
     }
+    
+    public List<Estado> findByIdEstado(final Integer idEstado) {
+        if (this.em != null && idEstado != null) {
+            Query q = em.createNamedQuery("Estado.findByIdEstado");
+            q.setParameter("idEstado", idEstado);
+            return q.getResultList();
 
+        }
+        return Collections.EMPTY_LIST;
+    }
+
+    public List<Estado> findNombre(String nombre) {
+
+        Query q = em.createNamedQuery("Estado.findByNombre");
+        q.setParameter("nombre", nombre);
+
+        return q.getResultList();
+    }
 }
